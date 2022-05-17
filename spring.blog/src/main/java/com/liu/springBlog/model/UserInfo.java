bddfede95030fbfbaafcb7afa6e5e3e523d0c781
liutianqiltq <liutianqitq@gmail.com>
2022-05-17 17:59:22 +0900

    6th
    userinfo

--- spring.blog/src/main/java/com/liu/springBlog/model/UserInfo.java
+++ spring.blog/src/main/java/com/liu/springBlog/model/UserInfo.java
@@ -15,8 +15,8 @@ public class UserInfo {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	public Long id;
-	public String name;
-	public String password;
+	private Long id;
+	private String name;
+	private String password;
 
 }
